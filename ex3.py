83c437e563bb408a054e8159b4923b0f26f53b5e
Harish-169-ps <dheenakishore169@gmail.com>
2025-11-10 10:11:44 +0530

    Duplicate code for electricity bill calculation

--- ex3.py
+++ ex3.py
@@ -4,8 +4,9 @@ print("-----------------")
 no1=input("enter the eb.no:")
 no2=input("enter the customer name:")
 no3=int(input("Reading for previous month:"))
-no4=int(input("REading for current month:"))
+no4=int(input("Reading for current month:"))
 total=no4-no3
 print("total unit consumed:",total)
 paid=total*5
 print("amount to be paid Rs:",paid)
+
